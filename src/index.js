2603ec9ffea9669f6811c6326b9773fcd114cb53
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-05-25 18:34:21 +0100

    Add corrections to linters error

--- src/index.js
+++ src/index.js
@@ -3,12 +3,3 @@ import _ from 'lodash';
 /* eslint-enable */
 
 import './style.css';
-
-const refreshBtn = document.getElementById('refresh');
-const submitBtn = document.getElementById('submit');
-const player = document.getElementById('name');
-const score = document.getElementById('score');
-const errorMessage = document.getElementById('error');
-const regex1 = /^\s+$/;
-const regex2 = /^[0-9]+$/;
-
